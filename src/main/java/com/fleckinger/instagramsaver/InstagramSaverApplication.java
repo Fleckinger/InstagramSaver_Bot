0d129c36ef07930cc5c27783f392520e76412750
Vladislav Safarov <egbertz@yandex.ru>
2022-01-13 18:43:57 +0300

    initial commit

---
+++ src/main/java/com/fleckinger/instagramsaver/InstagramSaverApplication.java
@@ -0,0 +1,13 @@
+package com.fleckinger.instagramsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramSaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fleckinger/instagramsaver/InstagramSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleckinger.instagramsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
