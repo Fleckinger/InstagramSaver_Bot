d78a257ac1337d5ad1a3df8d2b25a057c0a7519c
Vladislav Safarov <egbertz@yandex.ru>
2022-01-13 23:04:19 +0300

    feat: add dummy bot

--- src/main/java/com/fleckinger/instagramsaver/bot/InstagramSaverBot.java
+++ src/main/java/com/fleckinger/instagramsaver/bot/InstagramSaverBot.java
@@ -1,2 +1,31 @@
-package com.fleckinger.instagramsaver.bot;public class InstagramSaverBot {
+package com.fleckinger.instagramsaver.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class InstagramSaverBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage());
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
 }
